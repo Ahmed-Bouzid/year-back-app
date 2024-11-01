e35451ca72fc2af864968e8c51cb4f5afec1dd0d
BOUZID Ahmed <ahmahm@hotmail.co.jp>
2024-11-01 13:10:24 +0100

    adding

--- routes/years.js
+++ routes/years.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var router = express.Router();
-let year = new Date().getFullYear().toString();
-console.log(typeof year);
+let year = new Date().getFullYear();
+console.log(year);
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.json({ Year: year });
+  res.json({ Year: year.toString() });
 });
 
 module.exports = router;
